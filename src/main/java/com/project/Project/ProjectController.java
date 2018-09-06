65261c6a9b91a2716299ade642947fe5f4b615bf
Cheo <cheorowzee83@gmail.com>
2018-09-06 17:11:40 -0400

    first commit for final project

---
+++ src/main/java/com/project/Project/ProjectController.java
@@ -0,0 +1,16 @@
+package com.project.Project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ProjectController {
+	
+	@RequestMapping("/")
+	public ModelAndView index()  {
+		return new ModelAndView("index");
+		
+	}
+
+}
