79cc453c721871e187fbbe409bda10b8e35b9f2e
Alex <acwoody89@gmail.com>
2018-09-07 15:23:23 -0400

    WATSON API WORKS...FOR NOW

---
+++ src/main/java/com/project/Project/WatsonController.java
@@ -0,0 +1,97 @@
+package com.project.Project;
+
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.NaturalLanguageUnderstanding;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.AnalysisResults;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.AnalyzeOptions;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.EntitiesOptions;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.Features;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.KeywordsOptions;
+import com.ibm.watson.developer_cloud.service.security.IamOptions;
+@Controller
+public class WatsonController {
+	@RequestMapping("/index")
+	public ModelAndView WatsonKeyword() {
+//		System.out.println("Please Work");
+//		
+//		NaturalLanguageUnderstanding service = new NaturalLanguageUnderstanding("2017-02-27");
+//		service.setApiKey("ROL_zkeMlylSOvTnli6WlmiNkhgS-MpAn5kZYEEfUzKz");
+////		service.setUsernameAndPassword("<username>", "<password>");
+//
+//		EntitiesOptions entities = new EntitiesOptions.Builder()
+//		  .sentiment(true)
+//		  .limit(1)
+//		  .build();
+//		Features features = new Features.Builder()
+//		  .entities(entities)
+//		  .build();
+//		AnalyzeOptions parameters = new AnalyzeOptions.Builder()
+//		  .url("www.cnn.com")
+//		  .features(features)
+//		  .build();
+//
+//		AnalysisResults results = service.analyze(parameters).execute();
+//		System.out.println(results);
+//		
+//		return new ModelAndView("index");
+		
+		
+		
+		
+		
+		
+		
+//		CloseableHttpClient httpClient = HttpClients.custom().setSSLHostnameVerifier(new NoopHostnameVerifier())
+//				.build();
+//		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+//		requestFactory.setHttpClient(httpClient);
+		
+		IamOptions options = new IamOptions.Builder().apiKey("ROL_zkeMlylSOvTnli6WlmiNkhgS-MpAn5kZYEEfUzKz").build();
+
+		NaturalLanguageUnderstanding service = new NaturalLanguageUnderstanding("2018-03-16", options);
+		service.setEndPoint("https://gateway-wdc.watsonplatform.net/natural-language-understanding/api");
+		
+		String text = "IBM is an American multinational technology " +
+				  "company headquartered in Armonk, New York, " +
+				  "United States, with operations in over 170 countries.";
+
+		EntitiesOptions entitiesOptions = new EntitiesOptions.Builder()
+				  .emotion(true)
+				  .sentiment(true)
+				  .limit(2)
+				  .build();
+
+				KeywordsOptions keywordsOptions = new KeywordsOptions.Builder()
+				  .emotion(true)
+				  .sentiment(true)
+				  .limit(2)
+				  .build();
+
+				Features features = new Features.Builder()
+				  .entities(entitiesOptions)
+				  .keywords(keywordsOptions)
+				  .build();
+
+				AnalyzeOptions parameters = new AnalyzeOptions.Builder()
+				  .text(text)
+				  .features(features)
+				  .build();
+
+				AnalysisResults response = service
+				  .analyze(parameters)
+				  .execute();
+				System.out.println(response);
+				RestTemplate restTemplate = new RestTemplate();
+				return new ModelAndView("index", "keyword", response);
+//				
+
+	}
+}
