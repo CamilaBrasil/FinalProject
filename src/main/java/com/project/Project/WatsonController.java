4aa40484601c321b162af09e15ae76451e9a0757
Cheo <cheorowzee83@gmail.com>
2018-09-10 11:33:10 -0400

    bug fixes

--- src/main/java/com/project/Project/WatsonController.java
+++ src/main/java/com/project/Project/WatsonController.java
@@ -1,8 +1,6 @@
 package com.project.Project;
 
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
