afbbad6b800078eaf43682075ea5103925723a4b
Alex <acwoody89@gmail.com>
2018-09-12 10:22:18 -0400

    watson stuff

--- src/main/java/com/project/Project/WatsonController.java
+++ src/main/java/com/project/Project/WatsonController.java
@@ -19,7 +19,7 @@ public class WatsonController {
 	@Value("${watsonapikey}")
 	private String watsonapikey;
 
-	@RequestMapping("/index")
+	@RequestMapping("/watson")
 	public ModelAndView WatsonKeyword() {
 //		System.out.println("Please Work");
 //		
@@ -54,9 +54,7 @@ public class WatsonController {
 		NaturalLanguageUnderstanding service = new NaturalLanguageUnderstanding("2018-03-16", options);
 		service.setEndPoint("https://gateway-wdc.watsonplatform.net/natural-language-understanding/api");
 
-		String text = "Turbonomic, the global leader in Workload Automation for Hybrid Cloud, an Inc Fastest Growing Company for three years and a Forbes Cloud 100 company is growing our cloud team to meet our customers journey to digital transformation. Did you know that Gartner predicts 83% of enterprise workloads will be in the cloud by 2020?  As part of our world-class engineering team, you will design/build features and enhancements for the Turbonomic Platform which is the control system to manage workload performance, costs, and efficiency in the public cloud.\r\n"
-				+ " \r\n"
-				+ "As a Senior Cloud Software Engineer, you will have the opportunity to make a massive impact and will be directly responsible for developing some of the most integral parts of the Turbonomic Platform that has already enabled over 2,000 customers to transform their businesses for the digital age.  We are continuously thinking about the future of IT and constantly innovating to address trends in emerging technology.  If you have a passion for cloud technologies, let’s talk!";
+		String text = "Java HTML the difficult Cheese was stolen by a mouse from inside Google's SQL Databases";
 
 		EntitiesOptions entitiesOptions = new EntitiesOptions.Builder().emotion(true).sentiment(true).limit(2).build();
 
@@ -65,9 +63,11 @@ public class WatsonController {
 		Features features = new Features.Builder().entities(entitiesOptions).keywords(keywordsOptions).build();
 
 		AnalyzeOptions parameters = new AnalyzeOptions.Builder().text(text).features(features).build();
-
+		
 		AnalysisResults response = service.analyze(parameters).execute();
 		System.out.println(response);
+		
+		System.out.println(response.getKeywords());
 		return new ModelAndView("index", "keyword", response);
 //				
 
