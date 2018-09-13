d8a55c06075284e63c80e65070d1bba5f3c23b8b
Camila Brasil <mila.brasil@gmail.com>
2018-09-13 11:18:40 -0400

    working on favJobs

--- src/main/java/com/project/Project/dao/JobsRepo.java
+++ src/main/java/com/project/Project/dao/JobsRepo.java
@@ -1,10 +1,16 @@
 package com.project.Project.dao;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.Project.entity.FavJobs;
+import com.project.Project.entity.User;
 
 public interface JobsRepo extends JpaRepository <FavJobs, Integer>{
+	
 
+	
 	
 }
--- src/main/java/com/project/Project/dao/UserRepo.java
+++ src/main/java/com/project/Project/dao/UserRepo.java
@@ -1,14 +1,18 @@
 package com.project.Project.dao;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.project.Project.entity.FavJobs;
 import com.project.Project.entity.User;
 
 public interface UserRepo extends JpaRepository<User, Integer>{
 
 	User findByEmail(String keyword);
+//	Optional<User> findById(Integer id);
+	List<FavJobs> findFavJobsListByUser_id(Integer id);
 	
 	
 }
